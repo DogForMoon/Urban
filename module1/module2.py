0b545e5555a972651396a6d9e38318dd979e42ef
wilfi <mihelev2@mail.ru>
2024-09-23 21:46:42 +0300

    Initial commit

--- module1/module2.py
+++ module1/module2.py
@@ -1,12 +1,10 @@
-from random import seed
+def trap(n):
+    password_ = ""
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            if n % (i + j) == 0:  # i + j - пара
+                password_ += f"{i}{j}"
+    return password_
+print(trap(10))
 
-def matrix(n, m, value):
-    a = list()
-    for _ in range(n):
-        a.append([value]*m)
-    return a
-
-
-result1 = matrix(2, 2, 0)
-print(result1)
-
+#12 13 23 ...
